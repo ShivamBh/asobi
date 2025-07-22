9911426930efa52086804d537762eca8a5fc479d
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2025-07-22 18:42:32 +0530

    fix: check env for aws creds before prompt, use local asobi file for getting/setting

--- src/services/configService.ts
+++ src/services/configService.ts
@@ -1,17 +1,22 @@
 import { join } from "path";
-import { Application, AwsCredentials, CredentialAnswers } from "../types";
+import {
+  Application,
+  AwsCredentials,
+  CredentialAnswers,
+  InfrastructureConfig,
+} from "../types";
 import inquirer from "inquirer";
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
+import { readFileSync } from "fs";
 
 export class ConfigService {
   protected readonly configDir: string;
-  protected readonly appsConfigPath: string;
-
+  private configFile: string;
   private awsCredentials: AwsCredentials | null = null;
 
   constructor() {
     this.configDir = join(process.cwd(), ".asobi");
-    this.appsConfigPath = join(this.configDir, "apps.json");
+    this.configFile = join(this.configDir, "asobi.json");
   }
 
   // Get aws credentials from object state or prompt
@@ -20,6 +25,21 @@ export class ConfigService {
       return this.awsCredentials;
     }
 
+    // Check env vars first
+    const accessKeyId = process.env.AWS_ACCESS_KEY_ID || "";
+    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || "";
+    const region = process.env.AWS_REGION || "";
+
+    if (accessKeyId && secretAccessKey) {
+      this.awsCredentials = {
+        accessKeyId,
+        secretAccessKey,
+        region,
+      };
+      return this.awsCredentials;
+    }
+
+    // If env vars are not found, prompt user for input
     const answers = await inquirer.prompt<CredentialAnswers>([
       {
         type: "input",
@@ -49,14 +69,16 @@ export class ConfigService {
   }
 
   // List all asobi apps
-  async listApps(): Promise<string[]> {
+  async listApps(): Promise<string> {
     try {
-      const data = await readFile(this.appsConfigPath, "utf-8");
-      const apps: Application[] = JSON.parse(data);
-      return apps.map((app) => app.name);
+      const data: InfrastructureConfig = JSON.parse(
+        await readFile(this.configFile, "utf-8")
+      );
+      return data.appName;
     } catch (e) {
       console.error(`Could not find any "asobi" apps`);
-      return [];
+      return "";
+      // process.exit(1)
     }
   }
 
@@ -94,6 +116,16 @@ export class ConfigService {
     ]);
 
     return parseInt(answers.port);
+  }
+
+  // Update config file
+  async updateConfigFile(input: InfrastructureConfig): Promise<void> {
+    await writeFile(this.configFile, JSON.stringify(input));
+  }
+
+  // Read from config file
+  async readFromConfigFile(): Promise<InfrastructureConfig> {
+    return JSON.parse(await readFile(this.configFile, { encoding: "utf-8" }));
   }
 
   // Get config
