ad5f0a3162a77833cc57b7af1e4bac768eb951a5
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2025-12-06 17:39:54 +0530

    test: added coverage and vitest ui as dev dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,13 +8,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
-      exclude: [
-        "node_modules/",
-        "test/",
-        "dist/",
-        "**/*.test.ts",
-        "src/index.ts",
-      ],
+      exclude: ["node_modules/", "test/", "dist/", "src/index.ts"],
       // thresholds: {
       //   lines:
       // },
